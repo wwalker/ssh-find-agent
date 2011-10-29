6009ff154a88ee0ed32a034cdf7d88041c2e5df6
Wayne Walker <wwalker@solid-constructs.com>
2011-10-29 16:03:46 -0500

    first draft, currently finds all the info but you must assemble and test the SSH_AUTH_SOCK settings

---
+++ ssh-find-agent.bash
@@ -0,0 +1,28 @@
+find_all_ssh_agent_sockets() {
+	find /tmp -type s -name agent.\* 2> /dev/null | grep '/tmp/ssh-.*/agent.*'
+}
+
+find_all_gpg_agent_sockets() {
+	find /tmp -type s -name S.gpg-agent 2> /dev/null | grep '/tmp/gpg-.*/S.gpg-agent'
+}
+
+find_all_gnome_keyring_agent_sockets() {
+	find /tmp -type s -name ssh 2> /dev/null | grep '/tmp/keyring-.*/ssh$'
+}
+
+find_all_agent_sockets() {
+	find_all_ssh_agent_sockets
+	find_all_gpg_agent_sockets
+	find_gpg_agent_processes
+	find_all_gnome_keyring_agent_sockets
+	find_gnome_keyring_agent_processes
+}
+
+find_gpg_agent_processes() {
+	pgrep gpg-agent
+}
+
+find_gnome_keyring_agent_processes() {
+	pgrep -f gnome-keyring-daemon
+}
+
