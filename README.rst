cd2eea3528fc69de0c760063aa01db26adb2bc5f
Wayne Walker <wwalker@solid-constructs.com>
2011-10-29 16:22:10 -0500

    Added copyright statement to README.rst and ssh-find-agent.bash files

--- README.rst
+++ README.rst
@@ -4,3 +4,5 @@ ssh-find-agent
 ssh-find-agent is a tool for locating existing ssh compatible agent processes (e.g., ssh-agent, gpg-agent, gnome-keyring); and, optionally, setting SSH_AUTH_SOCK accordingly.
 
 ssh-find-agent is written in bash due to it's near ubiquitousness.
+
+Copyright (C) 2011 by Wayne Walker <wwalker@solid-constructs.com>
--- ssh-find-agent.bash
+++ ssh-find-agent.bash
@@ -1,3 +1,9 @@
+# Copyright (C) 2011 by Wayne Walker <wwalker@solid-constructs.com>
+#
+# Released under one of the versions of the MIT License.
+#
+# See LICENSE for details
+
 find_all_ssh_agent_sockets() {
 	find /tmp -type s -name agent.\* 2> /dev/null | grep '/tmp/ssh-.*/agent.*'
 }
@@ -10,19 +16,19 @@ find_all_gnome_keyring_agent_sockets() {
 	find /tmp -type s -name ssh 2> /dev/null | grep '/tmp/keyring-.*/ssh$'
 }
 
-find_all_agent_sockets() {
-	find_all_ssh_agent_sockets
-	find_all_gpg_agent_sockets
-	find_gpg_agent_processes
-	find_all_gnome_keyring_agent_sockets
-	find_gnome_keyring_agent_processes
-}
-
 find_gpg_agent_processes() {
 	pgrep gpg-agent
 }
 
 find_gnome_keyring_agent_processes() {
 	pgrep -f gnome-keyring-daemon
+}
+
+find_all_agent_sockets() {
+	find_all_ssh_agent_sockets
+	find_all_gpg_agent_sockets
+	find_gpg_agent_processes
+	find_all_gnome_keyring_agent_sockets
+	find_gnome_keyring_agent_processes
 }
 
