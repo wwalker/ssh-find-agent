dc012e6cf5d8792ca26b33870459638931fdffce
Chris McClimans <c@vulk.coop>
2016-05-16 23:15:12 +0000

    removing some whitespace

--- ssh-find-agent.sh
+++ ssh-find-agent.sh
@@ -3,17 +3,17 @@
 # Released under one of the versions of the MIT License.
 #
 # Copyright (C) 2011 by Wayne Walker <wwalker@solid-constructs.com>
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -135,7 +135,7 @@ find_all_agent_sockets() {
 	then
 		i=0
 		for a in $_LIVE_AGENT_LIST ; do
-			sock=${a/:*/} 
+			sock=${a/:*/}
 			_LIVE_AGENT_SOCK_LIST[$i]=$sock
 			akeys=$(SSH_AUTH_SOCK=$sock ssh-add -l) 
 			printf "%i) %s\n\t%s\n" $((i+1)) "$a" "$akeys"
@@ -180,7 +180,7 @@ ssh-find-agent() {
 		set_ssh_agent_socket -c
 	elif [ "$1" = "-a" -o "$1" = "--auto" ]
 	then
-		set_ssh_agent_socket 
+		set_ssh_agent_socket
 	else
 		find_all_agent_sockets -i
 	fi
